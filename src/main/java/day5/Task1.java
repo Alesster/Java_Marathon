37ff092e6790c1d7354f6f4612d6c5a1ee14affa
Alesster <aless.ter@gmail.com>
2023-02-19 00:42:19 +0100

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,13 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setModel("Lexus");
+        car1.setColor("White");
+        car1.setYear(2008);
 
+        System.out.println(car1.getModel());
+        System.out.println(car1.getColor());
+        System.out.println(car1.getYear());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,9 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorbike motorbike1 = new Motorbike("Honda", "Red", 2016);
+        System.out.println(motorbike1.getModel());
+        System.out.println(motorbike1.getColor());
+        System.out.println(motorbike1.getYear());
     }
 }
