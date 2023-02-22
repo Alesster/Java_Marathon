584a97a8e70403ee72e1b0e2b79d707263c8a583
Alesster <aless.ter@gmail.com>
2023-02-22 23:14:50 +0100

    complete day9 tasks

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,12 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        Student student = new Student("Kim Pupkin", "Art group");
+        Teacher teacher = new Teacher("Vasilisa Varum", "Art of flight");
 
+        System.out.println(student.getGroupName());
+        System.out.println(teacher.getSubjectName());
+        student.printInfo();
+        teacher.printInfo();
     }
 }
